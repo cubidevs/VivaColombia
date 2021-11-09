1048e167a122527a3c5b2e1e1a4520d6218898b1
juan jose botero <juanbotero037@gmail.com>
2021-11-08 22:24:31 -0500

    creacion actividad

---
+++ app/src/main/java/com/cubidevs/vivacolombia/DetallePereiraActivity.kt
@@ -0,0 +1,11 @@
+package com.cubidevs.vivacolombia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetallePereiraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pereira)
+    }
+}
