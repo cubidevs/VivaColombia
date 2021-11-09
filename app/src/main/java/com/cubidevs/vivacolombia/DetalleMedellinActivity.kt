5b0b310a9a442880fb2db0ed96120f76bf39dd82
edwinacubillos <edwinacubillos@gmail.com>
2021-11-08 22:10:38 -0500

    Creación de la actividad de Medellin

---
+++ app/src/main/java/com/cubidevs/vivacolombia/DetalleMedellinActivity.kt
@@ -0,0 +1,11 @@
+package com.cubidevs.vivacolombia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleMedellinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_medellin)
+    }
+}
