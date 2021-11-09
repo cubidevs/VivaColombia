926122a6be0c4c79c0d1f26a79aa73f1b5a5c833
pipee9324 <pipecubillos9324@gmail.com>
2021-11-09 13:07:18 -0500

    creacion de la actividad ibague

---
+++ app/src/main/java/com/cubidevs/vivacolombia/DetalleIbagueActivity.kt
@@ -0,0 +1,11 @@
+package com.cubidevs.vivacolombia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleIbagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_ibague)
+    }
+}
