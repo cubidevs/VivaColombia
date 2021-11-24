0dc7267a41c8428bfb4d7ddbf3498d07d0434daa
Juan Diego Ocampo C <juandego193@gmail.com>
2021-11-24 17:53:26 -0500

    agregando sitios turisticos avance

--- app/src/main/java/com/cubidevs/vivacolombia/RegistroTuristicoActivity.kt
+++ app/src/main/java/com/cubidevs/vivacolombia/RegistroTuristicoActivity.kt
@@ -2,10 +2,18 @@ package com.cubidevs.vivacolombia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class RegistroTuristicoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_turistico)
+
+        var registrarButton: Button = findViewById(R.id.registrar_button)
+        var nombreEditText: EditText = findViewById(R.id.nombre_edit_text)
+        var infoTextView: TextView = findViewById(R.id.info_text_view)
+
     }
 }
